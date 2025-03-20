3f5bdb40eb49bf40d950e54f2a27f90be238a589
bfyqjzsgkqycf <bfyqjzsgkqycf@gmail.com>
2025-03-20 11:20:26 +0800

    feat: 添加玩家子弹类
    添加了玩家子弹类PlayerBullet，继承自GameObject。实现了子弹的构造方法、移动方法和绘制方法。子弹在移动出屏幕后会从画面消失。新增了激活方法，用于在玩家位置初始化子弹。

--- src/model/PlayerBullet.java
+++ src/model/PlayerBullet.java
@@ -1,4 +1,34 @@
 package model;
 
-public class PlayerBullet {
+import java.awt.*;
+
+import main.*;
+
+// 玩家子弹类
+public class PlayerBullet extends GameObject {
+
+    public PlayerBullet() { // 玩家子弹构造方法
+        active = false;
+    }
+
+    public void move() { // 玩家子弹的移动
+        y -= 15;
+        // 移动出屏幕后从画面消失
+        if ((y < 0)) {
+            active = false;
+        }
+    }
+
+    public void draw(Graphics g) { // 循环一次执行一次
+        g.setColor(Color.blue);
+        // 绘制长方形
+        g.drawRect((int) x - 3, (int) y - 10, (int) 6, (int) 20);
+    }
+
+    // 在玩家位置进行初始化
+    public void activate(double ix, double iy) {
+        x = ix;
+        y = iy;
+        active = true; // 可视化子弹
+    }
 }
