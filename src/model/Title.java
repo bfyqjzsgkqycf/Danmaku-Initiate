aa35884c65f29f6e202539b99dbe8ac829084370
bfyqjzsgkqycf <bfyqjzsgkqycf@gmail.com>
2025-03-20 10:34:06 +0800

    feat: 添加标题和游戏结束画面绘制功能
    添加了标题和游戏结束画面的绘制方法，包括字体类的实例化和动画计数器。游戏开始界面显示“Micro Danmaku”和“Press SPACE”，游戏结束界面显示“GAMEOVER”。

--- src/model/Title.java
+++ src/model/Title.java
@@ -1,4 +1,37 @@
 package model;
 
+import java.awt.*;
+
+// 标题和游戏结束画面绘制
 public class Title {
+    int count; // 动画计数器
+    Font titleFont;
+    Font infoFont;
+
+
+    public Title() { // 为标题实例化字体类
+        count = 0;
+        titleFont = new Font("SimHei", Font.BOLD, 20);
+        infoFont = new Font("SimHei", Font.BOLD, 10);
+    }
+
+
+    public void drawTitle(Graphics g) { // 游戏开始界面
+        g.setColor(Color.white);
+        count++;
+        g.setFont(titleFont);
+        g.drawString("Micro Danmaku", 170, 150);
+        g.drawString("Press SPACE", 180, 350);
+
+    }
+
+
+    public void drawGameover(Graphics g) { // 游戏结束界面
+        g.setColor(Color.white);
+        count++;
+        g.setFont(titleFont);
+        g.drawString("GAMEOVER", 200, 150);
+    }
+
 }
+
