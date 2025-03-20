a71c7c8259e4cc754161ca9e9aa0c832bde639b0
bfyqjzsgkqycf <bfyqjzsgkqycf@gmail.com>
2025-03-20 10:56:39 +0800

    feat: 添加关卡管理功能
    增加关卡管理类Level，实现每500帧提高一关，提供获取当前关卡、增加关卡和初始化恢复关卡的方法。最高关卡为10关。

--- src/model/Level.java
+++ src/model/Level.java
@@ -1,4 +1,20 @@
 package model;
 
-public class Level {
+public class Level { // 关卡管理, 每 500帧 提高一关
+    static int level;
+
+    public static int getLevel() {
+        return level;
+    }
+
+    public static void addLevel() { // 关卡增加方法, 最高 10关
+        if (level < 8) {
+            level++;
+        }
+    }
+
+    public static void initLevel() // 初始化恢复等级
+    {
+        level = 0;
+    }
 }
