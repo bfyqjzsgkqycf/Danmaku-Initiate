c938f3d52df7ac0f00d1da226d681a9e72dc895a
bfyqjzsgkqycf <bfyqjzsgkqycf@gmail.com>
2025-03-20 10:51:35 +0800

    feat: 添加得分控制和显示功能
    添加了得分控制和显示的相关代码，包括得分和最高分的初始化、显示、计分添加和最高分更新处理。具体修改如下：
    - 在Score类中添加了静态变量myscore和hiscore，用于存储当前得分和最高分
    - 添加了Font对象scoreFont，用于设置得分显示的字体
    - 实现了Score类的构造函数，初始化字体和当前得分
    - 实现了drawScore方法，用于在屏幕上显示当前得分
    - 实现了drawHiScore方法，用于在屏幕上显示最高分
    - 实现了addScore方法，用于增加当前得分
    - 实现了compareScore方法，用于比较当前得分和最高分，并更新最高分
    - 实现了saveScore和loadScore方法，目前为空方法，用于后续保存和加载得分
    - 实现了initScore方法，用于初始化当前得分为0

--- src/model/Score.java
+++ src/model/Score.java
@@ -1,4 +1,51 @@
 package model;
 
+import java.awt.*;
+
+// 得分的控制和显示
 public class Score {
+    static int myscore;
+    static int hiscore;
+    Font scoreFont;
+
+    public Score() {
+        scoreFont = new Font("SimHei", Font.BOLD, 15);
+        myscore = 0;
+    }
+
+    public void drawScore(Graphics g) { // 得分显示
+        g.setColor(Color.white);
+        g.setFont(scoreFont);
+        g.drawString("得分:" + myscore, 10, 30);
+    }
+
+    public void drawHiScore(Graphics g) { // 最高分显示
+        g.setColor(Color.white);
+        g.setFont(scoreFont);
+        g.drawString("最高分:" + hiscore, 350, 30);
+    }
+
+    public static void addScore(int gain) { // 计分添加
+        myscore += gain;
+    }
+
+    public static void compareScore() { // 最高分更新处理
+        if (myscore > hiscore) {
+            hiscore = myscore;
+            saveScore();
+        }
+    }
+
+    static void saveScore() {
+
+    }
+
+    public static void loadScore() {
+
+    }
+
+
+    public static void initScore() {
+        myscore = 0;
+    }
 }
